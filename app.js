7d1cf11dbc5b46fb140178c056d84ddfa70bfedf
gonzalezvar <71093867+gonzalezvar@users.noreply.github.com>
2024-06-17 15:14:00 +0000

    datos a comparar js vs py

---
+++ app.js
@@ -0,0 +1,21 @@
+let typeString = "Esto es un string"; // string
+let typeNumber = 1;                     // number
+let typeObject = {                   //objeto
+    name:userName,
+    age:number
+};
+
+let typeBool = false;
+let typeNull = null;
+
+let typeArray = []
+
+//funciones
+
+function hello(){
+
+}
+
+const  helloArrayFunction = () => {
+
+}
